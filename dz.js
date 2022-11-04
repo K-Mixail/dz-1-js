4f47c5049740120e101d6f11aa628acbd320cdea
K-Mixail <131530@mail.ru>
2022-11-04 20:04:08 +0300

    добавил в репозиторий ранее созданный файл с дз

---
+++ dz.js
@@ -0,0 +1,64 @@
+console.log("Hello World");
+//document.write("Hello World");
+
+//number
+let pageCount = 32;
+console.log(pageCount);
+console.log(typeof pageCount);
+console.log(typeof(pageCount));
+
+//bigint
+const numberGoogol = 10**100;
+console.log(numberGoogol);
+console.log(typeof numberGoogol);
+console.log(typeof(numberGoogol));
+
+//string
+let nameYourCity = "Нижний Новгород";
+console.log(nameYourCity);
+console.log(typeof nameYourCity);
+console.log(typeof(nameYourCity));
+
+//boolean
+let isLess = 3 < 2;
+console.log (isLess);
+console.log(typeof isLess);
+console.log(typeof(isLess));
+
+
+//Null
+let userName = null;
+console.log(userName);
+console.log(typeof userName);
+console.log(typeof(userName));
+
+//Undefined
+let age;
+console.log(age);
+console.log(typeof age);
+console.log(typeof(age));
+
+
+//Symbol
+let id = Symbol("id");
+console.log(id);
+// console.log(id.toString());
+// console.log(id.discription);
+console.log(typeof id);
+console.log(typeof(id));
+
+
+//Object
+let laptopAcer = {
+    price: 30000,
+    OS: "windows",
+    ScreenDiagonal: 15.6
+}
+console.log(laptopAcer);
+console.log(typeof laptopAcer);
+console.log(typeof(laptopAcer));
+
+// const a = 7;
+// a++;
+// console.log(a);
+
